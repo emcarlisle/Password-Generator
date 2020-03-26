a8ea7eb1bbec7d0e0ce2c2535e9ab864fb71f80a
Emily Carlisle <emilydeluna96@gmail.com>
2020-03-25 19:17:08 -0500

    writing prompts

--- Assets/script.js
+++ Assets/script.js
@@ -15,6 +15,19 @@ generateBtn.addEventListener("click", writePassword);
 
 //declare function
 function generatepassword() {
+// code goes here
+// prompt user for "would you like to make a new password?"
+//confirm("Would you like to make a new password?")
+// prompt user length (8-120); confirm
+//prompt user upper case; confirm
+// prompt user for lower case; confirm
+// prompt user for numbers (1-9); confirm
+// prompt user for special characters (!,$,&); confirm
 
-  
+// declare variables
+var password = confirm("Would you like to make a new password?");
+var passwordLength = prompt("How long would you like your new password to be? (6-120 characters)");
+var lowerCase = confirm("Include lower case letters?");
+var upperCase = confirm("Include upper case letters?");
+
 }
