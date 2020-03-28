87f5375ddc5bc4ff1e82a8c28f0673b6f6bf54d3
Emily Carlisle <emilydeluna96@gmail.com>
2020-03-27 19:02:57 -0500

    redoing variables

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,72 +1,49 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//declare variables outside of function so that they can be used anytime (global) as arrays
+// and not inside function since they can only be used within that function (local)
+// when button is clicked, prompt user include numbers; confirm
+//prompt user include upperCase; confirm
+//prompt user include special chars; confirm
+//prompt user for passwordLength 8-128; prompt
+//variables for confirmed criteria
+//array to contain confirmed criteria
+//for loop for passwordLength
+//criteria requirements
+var num = ['0','1','2','3','4','5','6','7','8','9'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specialChar = ['!','@','#','$','%','&','-','+','=','*','?'];
 
-function generatePassword() {
-  //code goes here
-  // alerts to pop up asking the following prompts
-  // prompt user include numbers; confirm
-  // prompt user length (8-120); confirm
-  //prompt user upper case; confirm
-  // prompt user for lower case; confirm
-  // prompt user for special characters (!,$,&); confirm
+// confirmed array of user choices
+var confirmNum = ["Would you like numbers in your password?"];
+var confirmUpperCase = ["Include upper case?"];
+var confirmSpecialChar = ["Include special characters?"];
+var confirmPasswordLength = prompt("How many characters would you like? Please enter a number between 8-128.");
 
-  // declare variables; write prompts
-  var password = confirm("Would you like to include numbers?"); // ok/cancel
-  var numbers = ['1','2','3','4','5','6','7','8','9'] // array of strings of numbers
-
-  
-
-  var passwordLength = prompt("How many characters between 8 and 128?"); // user types num 8-128
-  
-  if (passwordLength < 8 || passwordLength > 128) {
-    // error message
-  }
-  if (typeof(passwordLength) !== numbers) {
-    // error message
-  }
-  
-  
-  var lowerCase = confirm("Include lower case letters?"); // ok/cancel
-  var upperCase = confirm("Include upper case letters?");
-  var symbols = confirm("Include special characters?");
-  
-  //create array to hold values user choices
-  var confirmedPassword = confirm("Are you satisfied with this password?");
-  var passwordArr = []; //holds value of user choices
-  // if statement of if all prompts confirmed
-  if () { // logic: if all prompts confirmed
-    passwordArr = passwordArr.concat(numbers) //concat adds values to existing/empty array
-  }
-  if () {
-    passwordArr = passwordArr.concat(passwordLength)
-  }
-  if () {
-    passwordArr = passwordArr.concat(upperCase)
-  }
-  if () {
-    passwordArr = passwordArr.concat(lowerCase)
-  }
-  if () {
-    passwordArr = passwordArr.concat(symbols)
-  }
-  return ' ';
+// if statment for confirmPL
+if (confirmPasswordLength < 8 || confirmPasswordLength > 128) {
+    alert("Please enter a valid number between given range.");
 }
+
+
+]
+function generatePassword() {
+    //code goes here
+
+    
+    var password = prompt()
+    return 
+  }
+
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
   
-// var options = [array of variables]; for loop for length of password
-// 
-
-
+  
+    passwordText.value = password;
+  
+  }
---
+++ Unsolved/script2.js
@@ -0,0 +1,72 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  //code goes here
+  // alerts to pop up asking the following prompts
+  // prompt user include numbers; confirm
+  // prompt user length (8-120); confirm
+  //prompt user upper case; confirm
+  // prompt user for lower case; confirm
+  // prompt user for special characters (!,$,&); confirm
+
+  // declare variables; write prompts
+  var password = confirm("Would you like to include numbers?"); // ok/cancel
+  var numbers = ['1','2','3','4','5','6','7','8','9'] // array of strings of numbers
+
+  
+
+  var passwordLength = prompt("How many characters between 8 and 128?"); // user types num 8-128
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    // error message
+  }
+  if (typeof(passwordLength) !== numbers) {
+    // error message
+  }
+  
+  
+  var lowerCase = confirm("Include lower case letters?"); // ok/cancel
+  var upperCase = confirm("Include upper case letters?");
+  var symbols = confirm("Include special characters?");
+  
+  //create array to hold values user choices
+  var confirmedPassword = confirm("Are you satisfied with this password?");
+  var passwordArr = []; //holds value of user choices
+  // if statement of if all prompts confirmed
+  if () { // logic: if all prompts confirmed
+    passwordArr = passwordArr.concat(numbers) //concat adds values to existing/empty array
+  }
+  if () {
+    passwordArr = passwordArr.concat(passwordLength)
+  }
+  if () {
+    passwordArr = passwordArr.concat(upperCase)
+  }
+  if () {
+    passwordArr = passwordArr.concat(lowerCase)
+  }
+  if () {
+    passwordArr = passwordArr.concat(symbols)
+  }
+  return ' ';
+}
+  
+// var options = [array of variables]; for loop for length of password
+// 
+
+
